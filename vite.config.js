f7aa750dff6b5daef7a42f255f7af8393ae71025
rasahil <sahilrangari31@gmail.com>
2025-05-29 21:32:58 +0530

    d

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+preview: {
+    // The --host 0.0.0.0 and --port $PORT are set by Render in the start command,
+    // so Vite will use those. You don't necessarily need to specify them here again
+    // unless you want to override for local `vite preview` testing.
+
+    allowedHosts: [
+      'pointse-loginapi.onrender.com',
+      // If you have a custom domain pointing to this Render service, add it here too.
+      // e.g., 'yourcustomdomain.com'
+    ],
+  }
 })
